3151a995960036e1473f398e947283f9ffdab66c
LeoSimonn <leonardo.monteiro004@edu.pucrs.br>
2023-11-07 11:47:50 -0300

    projetofinalizado

---
+++ apiswift/CharacterView.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterView.swift
+//  apiswift
+//
+//  Created by Foundation15 on 07/11/23.
+//
+
+import Foundation
---
+++ apiswift/CharactersListView.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersListView.swift
+//  apiswift
+//
+//  Created by Foundation15 on 07/11/23.
+//
+
+import Foundation
