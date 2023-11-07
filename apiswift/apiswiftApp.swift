967f23a8e3d65783e34ab3049fa29d764649adce
LeoSimonn <leonardo.monteiro004@edu.pucrs.br>
2023-11-07 10:05:16 -0300

    primeiros arquivos

---
+++ apiswift/apiswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiswiftApp.swift
+//  apiswift
+//
+//  Created by Foundation15 on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct apiswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
